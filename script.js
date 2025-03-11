ea0b640b5b175b7d03f22f7bbe16118403cd17d9
Cai <caiowain@gmail.com>
2025-03-11 16:17:03 +0100

    updated

--- script.js
+++ script.js
@@ -49,3 +49,13 @@ function initializePage() {
         }, 100);
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navbar = document.querySelector('.navbar');
+
+    menuToggle.addEventListener('click', function () {
+        menuToggle.classList.toggle('active');
+        navbar.classList.toggle('active');
+    });
+});
